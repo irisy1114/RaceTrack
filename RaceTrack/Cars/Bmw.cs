6bb42c1e9c78390c8cc08478f8ae0fccfdc9450b
irisy <ayao@my.wctc.edu>
2022-03-07 19:13:06 -0600

    Add project files.

---
+++ RaceTrack/Cars/Bmw.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Bmw : RaceCar
+    {
+        public Bmw()
+        {
+            Name = "BMW";
+            TopSpeed = 100;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} is ready to hit the road!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} is ready to stop!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} cuts the engine!");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/SuperDad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class SuperDad : Driver
+    {
+        public SuperDad(RaceCar car) : base(car)
+        {
+            Name = "SuperDad";
+            SkillLevel = 11;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
